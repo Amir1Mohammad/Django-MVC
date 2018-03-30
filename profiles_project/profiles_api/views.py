4b881667e7bd586ddddcb1999713c6aaffbbcfb2
Elk Cloner <Elk Cloner>
2018-03-31 04:10:59 +0430

    Added profile listing and creating feature.

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -104,3 +104,15 @@ class HelloViewSet(viewsets.ViewSet):
         """Handles removing an object."""
 
         return Response({'http_method': 'DELETE'})
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, creating and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (permissions.UpdateOwnProfile,)
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ('name', 'email',)
